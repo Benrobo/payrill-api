dbcffc8322fd7233c79b306e1088341671a06a7f
Benrobo <alumonabenaiah71@gmail.com>
2022-09-05 17:43:25 +0100

    updated config

--- src/services/db.js
+++ src/services/db.js
@@ -1,19 +1,27 @@
-require("dotenv").config({ path: "./.env" });
+const mysql = require("mysql");
 
+require("dotenv").config({ path: "./.env" });
 let host = process.env.HOST;
 let user = process.env.DB_USER;
 let password = process.env.DB_PASSWORD;
 let database = process.env.DATABASE;
 
-let mysql = require("mysql");
+
+
 const connection = mysql.createConnection({
     host: host || "localhost",
     user: user || "root",
-    password: password || "root",
+    password: password || "",
     database: database || "payrill",
-    port: '/var/run/mysqld/mysqld.sock'  // hack
+    // port: '/var/run/mysqld/mysqld.sock'  // hack
 });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log("DATABASE CONNECTED")
+});
 
 module.exports = connection;
+
