3ddf3f7388cc5393d165c3514080317502c8f132
Benrobo <alumonabenaiah71@gmail.com>
2022-09-10 16:00:12 +0100

    feat: added getting of store by subdomain

--- src/controller/store.controller.js
+++ src/controller/store.controller.js
@@ -105,6 +105,122 @@ class StoreController {
             }
         );
     }
+
+    async getAllStores(res) {
+        try {
+            const { id } = res.user;
+            const query = `SELECT * FROM stores WHERE (user_id = ?)`;
+            db.query({
+                sql: query,
+                values: [id],
+                timeout: 4000
+            }, (error, results, fields) => {
+                if (error) {
+                    return sendResponse(
+                        res,
+                        400,
+                        false,
+                        "Something went wrong getting stores",
+                    );
+                }
+
+                return sendResponse(
+                    res,
+                    200,
+                    true,
+                    "store retrieved successfully",
+                    results
+                );
+            })
+        }
+        catch (e) {
+            console.log(e)
+            return sendResponse(
+                res,
+                500,
+                false,
+                "something went wrong retrieving store",
+            );
+        }
+    }
+
+    async getStoreByDomain(res, isAuthenticated, domain) {
+        const query = isAuthenticated ? `SELECT id,name,subdomain,verified,location,description,logo,cover_photo FROM stores WHERE (user_id = ? AND subdomain = ?)` : `SELECT id,name,subdomain,verified,theme_bg,theme_color,location,description,logo,cover_photo FROM stores WHERE (subdomain = ?)`;
+
+        // for authenticated users
+
+        if (isAuthenticated) {
+            const { id } = res.user;
+            db.query({
+                sql: query,
+                values: [id, domain],
+                timeout: 4000
+            }, (error, results, fields) => {
+                if (error) {
+                    return sendResponse(
+                        res,
+                        400,
+                        false,
+                        "Something went wrong getting stores",
+                    );
+                }
+
+                if (results.length === 0) {
+                    return sendResponse(
+                        res,
+                        404,
+                        false,
+                        "Not found",
+                        results
+                    );
+                }
+
+                return sendResponse(
+                    res,
+                    200,
+                    true,
+                    "store retrieved successfully",
+                    results[0]
+                );
+            })
+            return;
+        }
+
+        // for non-authenticated users
+
+        db.query({
+            sql: query,
+            values: [domain],
+            timeout: 4000
+        }, (error, results, fields) => {
+            if (error) {
+                return sendResponse(
+                    res,
+                    400,
+                    false,
+                    "Something went wrong getting stores",
+                );
+            }
+
+            if (results.length === 0) {
+                return sendResponse(
+                    res,
+                    404,
+                    false,
+                    "Not found",
+                    results
+                );
+            }
+
+            return sendResponse(
+                res,
+                200,
+                true,
+                "store retrieved successfully",
+                results[0]
+            );
+        })
+    }
 }
 
 module.exports = StoreController;
